6452642535fcbc7b5588825ca08ff65a73a307a8
Luis Carbajal <luis.carbajal@wizeline.com>
2021-01-19 10:51:11 -0600

    feat: sanitize log output, removes all special characters from the message before logging it

--- logger.py
+++ logger.py
@@ -2,6 +2,7 @@
 Default logging module for Yasna
 """
 import logging
+import re
 
 from os import environ
 
@@ -10,6 +11,28 @@ logging.basicConfig(format=FORMAT)
 logger = logging.getLogger("yasna.main")
 logger.setLevel(environ.get("LOGLEVEL", "INFO"))
 
+class SanitizeFormatter(object):
+    """
+    SanitizeFormatter
+    """
+    def __init__(self, orig_formatter):
+        self.orig_formatter = orig_formatter
+
+    def format(self, record):
+        """
+        Removes any special character from
+        the log message to prevent any malicious
+        code that could help to make commands
+        """
+        message = self.orig_formatter.format(record)
+        message = re.sub(r'/[^A-Za-zñÑ0-9\s\.\,]*', '', message)
+        return message
+
+    def __getattr__(self, attr):
+        return getattr(self.orig_formatter, attr)
+
+for h in logging.root.handlers:
+    h.setFormatter(SanitizeFormatter(h.formatter))
 
 def get_logger(name):
     """
