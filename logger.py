41fc895cb3eab1507f0ffb44130095f87c4cf044
Luis Carbajal <luis.carbajal@wizeline.com>
2021-02-10 22:31:22 -0600

    feat: format with black

--- logger.py
+++ logger.py
@@ -11,10 +11,12 @@ logging.basicConfig(format=FORMAT)
 logger = logging.getLogger("yasna.main")
 logger.setLevel(environ.get("LOGLEVEL", "INFO"))
 
+
 class SanitizeFormatter(object):
     """
     SanitizeFormatter
     """
+
     def __init__(self, orig_formatter):
         self.orig_formatter = orig_formatter
 
@@ -25,14 +27,16 @@ class SanitizeFormatter(object):
         code that could help to make commands
         """
         message = self.orig_formatter.format(record)
-        message = re.sub(r'/[^A-Za-zñÑ0-9\s\.\,]*', '', message)
+        message = re.sub(r"/[^A-Za-zñÑ0-9\s\.\,]*", "", message)
         return message
 
     def __getattr__(self, attr):
         return getattr(self.orig_formatter, attr)
 
+
 for h in logging.root.handlers:
     h.setFormatter(SanitizeFormatter(h.formatter))
+
 
 def get_logger(name):
     """
